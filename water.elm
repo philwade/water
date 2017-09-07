e3eb9d7534a129d925076d123820a56057da7d2d
Phil Wade <phil@whoisstudio.com>
2017-09-07 15:28:34 -0400

    Make it look nice

--- water.elm
+++ water.elm
@@ -1,5 +1,6 @@
-import Html exposing (Html, div, text, span, button)
+import Html exposing (Html, div, text, span, button, img)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class, attribute)
 
 main = Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
@@ -18,8 +19,8 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ displayCount model
-           , button [ onClick Increment ] [ text "+" ]
+    div [ class "display" ] [ displayCount model
+           , button [ class "increment", onClick Increment ] [ text "+" ]
            , progressDisplay model
            , encouragementDisplay model
            ]
@@ -27,13 +28,13 @@ view model =
 displayCount : Model -> Html Msg
 displayCount model =
     if model.count == 0 then
-        div [] [ text "Nothing yet, get drinking!" ]
+        div [ class "display-text" ] [ text "Nothing yet, get drinking!" ]
     else
-        div [] (List.repeat model.count waterImage)
+        div [ class "display-emoji" ] (List.repeat model.count waterImage)
 
 progressDisplay : Model -> Html Msg
 progressDisplay model =
-    div [] [ text <| (toString model.count) ++ " / " ++ (toString model.goal) ]
+    div [ class "progress" ] [ text <| (toString model.count) ++ " / " ++ (toString model.goal) ]
 
 encouragementDisplay : Model -> Html Msg
 encouragementDisplay model =
@@ -41,16 +42,16 @@ encouragementDisplay model =
         halfway = model.goal // 2
     in
         if model.count == halfway then
-            div [] [ text "halfway there! you got this!" ]
+            div [ class "comment-half" ] [ text "halfway there! you got this!" ]
         else if model.count == model.goal then
-            div [] [ text "you did it! hell yeah!" ]
+            div [ class "comment-success"] [ text "you did it! hell yeah!" ]
         else if model.count > model.goal then
             div [] [ text "awwww damn getting extracurricular!" ]
         else
             span [] []
 
 waterImage : Html Msg
-waterImage = span [] [ text "🚰" ]
+waterImage = span [] [ img [ class "water-img", attribute "src" "water.png" ] [] ]
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
